20863a23540e3a415711d6a378ce595a58c08cb4
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-04-08 21:11:58 +0100

    Completed Ex.3 - though could not make all of the prices line up exactly

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,51 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+
+
+const [itemName, quantity, unitPrice] = order;
+
+
+function orderedItems(order){
+  
+  let arr = [];
+  let itemTotal;
+
+  for(item of order){
+    itemTotal = (item.unitPrice * item.quantity);
+    arr.push(itemTotal);
+  }
+  return arr;
+}
+
+const costArray = orderedItems(order);
+
+
+function orderTotal(costArray){
+  let total = 0;
+
+  costArray.forEach(price => {
+    total += price;
+  })
+
+  return total;
+}
+
+const total = orderTotal(costArray);
+
+
+function receipt(order){
+  
+  const receiptHeader = 'QTY     ITEM                TOTAL';
+  console.log(receiptHeader);
+
+  for(item of order){
+    console.log(`${item.quantity}       ${item.itemName}           ${(((item.unitPrice * item.quantity) * 100) / 100)
+    .toFixed(2)}`);
+  }
+  console.log('');
+  console.log(`Total: ${total}`);
+}
+
+receipt(order);
---
+++ exercises/exercise-3/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+function receipt(order){
+  
+//   const receiptHeader = 'QTY     ITEM                TOTAL';
+//   console.log(receiptHeader);
+
+//   for(item of order){
+   
+//     console.log(`${item.quantity}       ${item.itemName}           `)
+//   }
+//   console.log('');
+//   console.log(`Total:`);
+// }
